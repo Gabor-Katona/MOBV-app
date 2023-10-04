b466f43317726d05ed827a43dc2612c84abbd187
gabor.katona <gabor.katana@refis.sk>
2023-10-04 21:04:37 +0200

    added styles, theme, material elements

--- app/src/main/java/eu/mcomputing/mobv/zadanie/LoginFragment.kt
+++ app/src/main/java/eu/mcomputing/mobv/zadanie/LoginFragment.kt
@@ -1,12 +1,16 @@
 package eu.mcomputing.mobv.zadanie
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
@@ -15,11 +19,15 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
         view.findViewById<Button>(R.id.submit_button).apply {
             setOnClickListener {
-                val username: String = view.findViewById<EditText>(R.id.edit_text_username).text.toString()
-                val password: String = view.findViewById<EditText>(R.id.edit_text_password).text.toString()
-                login(username, password)
+                val username: String = view.findViewById<TextInputEditText>(R.id.edit_text_username).text.toString()
+                val password: String = view.findViewById<TextInputEditText>(R.id.edit_text_password).text.toString()
+
+                Log.d("TAG", username)
+                Log.d("TAG", password)
+                login(username, password, view)
             }
         }
+
 
         view.findViewById<Button>(R.id.reset_passwd_button).apply {
             setOnClickListener {
@@ -29,7 +37,12 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
     }
 
-    private fun login(username: String, password: String) {
+    private fun login(username: String, password: String, view: View) {
+        Snackbar.make(view, "Toto je Snackbar", Snackbar.LENGTH_LONG)
+            .setAction("Akcia") {
+                // kód po stlačení akcie na Snackbar
+            }.show()
+
         findNavController().navigate(R.id.action_login_feed)
     }
 }
